4320af9e66a6dda5f8d87de2d77c8a366e9524c0
udaikue <ikueikue@gmail.com>
2020-10-28 10:35:10 +0900

    deviseを使ってユーザー認証を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
+  # ログインしていなければログイン画面にリダイレクト
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -11,5 +14,34 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
+  end
+
+  def after_sign_in_path_for(_resource)
+    # ログイン後に遷移するpathを設定
+    users_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    # ログアウト後に遷移するpathを設定
+    new_user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+        :username,
+        :email,
+        :zipcode,
+        :address,
+        :profile
+    ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+        :username,
+        :email,
+        :zipcode,
+        :address,
+        :profile
+    ])
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
-  before_action :set_book, only: %I[show edit update destroy]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
     @books = Book.page(params[:page]).per(10)
