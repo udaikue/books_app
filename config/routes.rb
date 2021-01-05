47bbf7aab5aada690ee2828b02eeba1ee2a47b73
udaikue <ikueikue@gmail.com>
2021-01-05 10:35:57 +0900

    %Iを小文字に修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
       end
     end
     resource :user_icons, only: :destroy
-    resources :relationships, only: %I[create destroy]
+    resources :relationships, only: %i[create destroy]
   end
 end
